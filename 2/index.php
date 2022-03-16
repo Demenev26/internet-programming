bb7ea110714d044667aaed2d0db4cbe73c1f0c86
Demenev26 <82088953+Demenev26@users.noreply.github.com>
2022-03-16 10:05:30 +0500

    Добавил 2 лабу
    Добавил 2 лабу

---
+++ 2/index.php
@@ -0,0 +1,19 @@
+<html>
+<head> <title> Лаба 2 </title> </head>
+<body align="center">
+<h1>Деменев Константин ПИ-323</h1>
+<u1>
+         <h2>Часть 1 - Работа с массивами</h2>
+        <li><a href="http://a0644086.xsph.ru/2/lab-2-1.php">Лаба-2-1</a></li>
+		<li><a href="http://a0644086.xsph.ru/2/lab-2-2.php">Лаба-2-2</a></li>
+		<li><a href="http://a0644086.xsph.ru/2/lab-2-3.php">Лаба-2-3</a></li>
+		<h2>САМОСТОЯТЕЛЬНАЯ РАБОТА</h2>
+		<li><a href="http://a0644086.xsph.ru/2/lab-2-4.php">Лаба-2-4</a></li>
+		<h2>Часть 2 - Функции пользователя</h2>
+        <li><a href="http://a0644086.xsph.ru/2/lab-2-5.php">Лаба-2-5</a></li>
+        <li><a href="http://a0644086.xsph.ru/2/lab-2-6.php">Лаба-2-6</a></li>
+		
+</u1>
+
+</body>
+</html>
