065c7c73a3da8e38b03c670b65d6f6a023bbcdd1
Demenev26 <82088953+Demenev26@users.noreply.github.com>
2022-03-16 09:38:57 +0500

    Добавил лабу 1
    Добавил лабу 1

---
+++ 1/index.php
@@ -0,0 +1,35 @@
+<html>
+<head> <title> Лаба 1 </title> </head>
+<p align="left"> Дата и время:
+    <?php
+$d=date("d.m.Y H:i");
+echo($d);
+?>
+<body align="center">
+<h1>Деменев Константин ПИ-323</h1>
+<u1>
+        <h2>Часть 1. Введение в PHP</h2>
+        <li><a href="http://a0644086.xsph.ru/1/lab-1-1.php">Лаба-1-1</a></li>
+		<li><a href="http://a0644086.xsph.ru/1/lab-1-2.php">Лаба-1-2</a></li>
+		<li><a href="http://a0644086.xsph.ru/1/lab-1-3.php">Лаба-1-3</a></li>
+		<li><a href="http://a0644086.xsph.ru/1/lab-1-4.php">Лаба-1-4</a></li>
+		<h2>Сам. работа № 1.</h2>
+        <li><a href="http://a0644086.xsph.ru/1/lab-1-5.php">Лаба-1-5 5 вариант</a></li>
+        <li><a href="http://a0644086.xsph.ru/1/lab-1-5.10.php">Лаба-1-5 10 вариант</a></li>
+        <h2>Часть 2. Управляющие конструкции</h2>
+		<li><a href="http://a0644086.xsph.ru/1/lab-1-6.php">Лаба-1-6</a></li>
+		<li><a href="http://a0644086.xsph.ru/1/lab-1-7.php">Лаба-1-7</a></li>
+		<li><a href="http://a0644086.xsph.ru/1/lab-1-8.php">Лаба-1-8</a></li>
+		<li><a href="http://a0644086.xsph.ru/1/lab-1-9.php">Лаба-1-9</a></li>
+		<li><a href="http://a0644086.xsph.ru/1/lab-1-10.php">Лаба-1-10</a></li>
+		<li><a href="http://a0644086.xsph.ru/1/lab-1-11.php">Лаба-1-11</a></li>
+		<li><a href="http://a0644086.xsph.ru/1/lab-1-12.php">Лаба-1-12</a></li>
+		<li><a href="http://a0644086.xsph.ru/1/lab-1-13.php">Лаба-1-13</a></li>
+		<li><a href="http://a0644086.xsph.ru/1/lab-1-14.php">Лаба-1-14</a></li>
+		<h2>Сам. работа  № 2. Действия с числами.</h2>
+		<li><a href="http://a0644086.xsph.ru/1/lab-1-15.php">Лаба-2-10</a></li>
+		<li><a href="http://a0644086.xsph.ru/1/lab-1-16.php">Лаба-2-11 (1 задача)</a></li>
+		<li><a href="http://a0644086.xsph.ru/1/lab-1-17.php">Лаба-2-11 (4 задача)</a></li>
+</u1>
+</body>
+</html>
